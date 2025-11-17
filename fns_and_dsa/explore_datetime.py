ad69180ce9e16efdc47c8d1e2731b8204865d977
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-11-17 21:05:52 +0300

    changed the prompt for days

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
     print(f'Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}')
 
 def calculate_future_date():
-    days = int(input("Enter the number of days: "))
+    days = int(input("Enter the number of days to add to the current date:"))
     future_date = datetime.now() + timedelta(days=days)
     print(f'Future date: {future_date.strftime("%Y-%m-%d")}')
 
