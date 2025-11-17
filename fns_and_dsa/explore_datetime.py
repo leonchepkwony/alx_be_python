baae193a7c44091c1302cc8d1f2b83f52384ad7c
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-11-17 21:04:07 +0300

    Explore `datetime` Module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f'Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}')
+
+def calculate_future_date():
+    days = int(input("Enter the number of days: "))
+    future_date = datetime.now() + timedelta(days=days)
+    print(f'Future date: {future_date.strftime("%Y-%m-%d")}')
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
