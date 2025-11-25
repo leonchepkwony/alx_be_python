09d298f2e015486e8b1fc8e069d1e763b1ce0eb5
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-11-25 12:18:00 +0300

    renamed variable denominator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
-def safe_divide(numerator, denomonater):
+def safe_divide(numerator, denominator):
     try:
-        return (float(numerator) / float(denomonater))
+        return (float(numerator) / float(denominator))
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
